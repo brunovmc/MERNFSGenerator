690514217025f14a03256dba6f72cb2044c900a9
brunovidigalcoriolano <bruno.vidigal.coriolano@gmail.com>
2023-05-20 17:36:46 -0300

    begin cleaning code proper indentation and function breakdown

--- mern_structure_generator.sh
+++ mern_structure_generator.sh
@@ -7,10 +7,6 @@ create_server_directory() {
     npm install express cors mongoose dotenv
     npm install --save-dev nodemon
     mkdir models public routes services
-}
-
-create_index_file() {
-    cd server
     cat <<EOF > index.js
 const express = require('express');
 const cors = require('cors');
@@ -25,21 +21,37 @@ app.use(express.json());
 mongoose.connect('mongodb://127.0.0.1:27017/$main_file_name');
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+    console.log('MongoDB database connection established successfully');
 });
 
 app.listen(port, () => {
-  console.log('Server is running on port: ' + port);
+    console.log('Server is running on port: ' + port);
 });
 EOF
+
     cd ..
 }
 
-if [ -z "$1" ]
-then
+create_react_app() {
+    npm install create-react-app
+    npx create-react-app client
+}
+
+create_readme() {
+    echo "# $main_file_name" > README.md
+    echo "" >> README.md
+    echo "## Basic Features" >> README.md
+    echo "Here are some of the basic features included in this project:" >> README.md
+    echo "- Feature 1: Node.js/Express server." >> README.md
+    echo "- Feature 2: MongoDB database." >> README.md
+    echo "- Feature 3: React-create-app for Frontend." >> README.md
+    echo "" >> README.md
+    echo "README file created successfully!"
+}
+
+if [ -z "$1" ]; then
     read -p "Please provide a main file name: " main_file_name
-    if [ -z "$main_file_name" ]
-    then
+    if [ -z "$main_file_name" ]; then
         echo "Error: Must provide a directory name"
         exit 1
     fi
@@ -47,12 +59,10 @@ else
     main_file_name=$1
 fi
 
-if [ $# -gt 1 ]
-  then
-    if ! [ -d "$2" ]
-      then
+if [ $# -gt 1 ]; then
+    if ! [ -d "$2" ]; then
         echo "Invalid path, creating in current directory"
-      else
+    else
         cd $2
     fi
 fi
@@ -62,11 +72,9 @@ cd $main_file_name
 
 touch .env .env.example
 npm init -y
-npm install create-react-app
-npx create-react-app client
 
+create_react_app
 create_server_directory
-create_index_file
 
 cd server/models
 cat <<EOF > index.js
@@ -74,12 +82,12 @@ const mongoose = require('mongoose');
 const ExampleSchema = require('./example');
 
 const connect = () => {
-  mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect(process.env.MONGO_URL);
 };
 
 module.exports = {
-  Example,
-  connect,
+    Example,
+    connect,
 };
 EOF
 
@@ -87,14 +95,14 @@ cat <<EOF > example.js
 const { Schema } = require('mongoose');
 
 const Example = new Schema({
-  id: {
-    type: Number,
-    required: true,
-  },
-  nome: {
-    type: String,
-    required: true,
-  },
+    id: {
+        type: Number,
+        required: true,
+    },
+    nome: {
+        type: String,
+        required: true,
+    },
 });
 
 module.exports = Example;
@@ -104,14 +112,14 @@ cd ../..
 
 cd server/routes
 mkdir api auth midddlewares
-echo $'const express = require("express");\n\nconst router = express.Router();\n\nrouter.get("/", (_, res) => {\n  res.send("<h1>Home</h1>");\n});\n\nmodule.exports = router;' > home.js
+echo $'const express = require("express");\n\nconst router = express.Router();\n\nrouter.get("/", (_, res) => {\n    res.send("<h1>Home</h1>");\n});\n\nmodule.exports = router;' > home.js
 
 cd ..
 touch auth/index.js
 cd ..
 
-
 sed -i '/exit 1\"/s/$/,\n    "start": "start cmd.exe \/c \\"cd client \&\& start cmd.exe \/k npm start\\" \& start cmd.exe \/c \\"cd server \&\& start cmd.exe \/k node index.js\\"" /' package.json
 
-echo "Basic MERN stack file structure created successfully!"
-echo "Script executed in $(($SECONDS / 3600))h$((($SECONDS / 60) % 60))m$(($SECONDS % 60))s"
+echo "Creating README file..."
+create_readme
+echo "Basic MERN stack file structure
